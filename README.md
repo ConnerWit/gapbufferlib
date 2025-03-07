cac365b3a362053c43117014ac49371fdd6b62ef
ConnerWit <97093629@st.deltion.nl>
2025-03-07 21:39:26 +0100

    openfile function saves file into string

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-this is a readme file
+README file temporarily is utilized for planning
+
+project requirements:
+1. being able to open files, make new files & edit files
+2. syntax highlighting for C, C++, Rust, Python, PHP, HTML & CSS
+3. special keybind shortcuts
+4. data structure for text storage [needs refinement]
--- main.c
+++ main.c
@@ -1,7 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+
+#define MAX_SIZE 10000
+
+int openFile(){
+    system("cls");
+
+    char ch[50];
+    scanf("%s", ch);
+    
+    FILE *file = fopen(ch, "rb");
+    if(!file) {
+        printf("no such file or directory");
+        return 1;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    rewind(file);
+
+    char *fstr = (char*)malloc(file_size + 1);
+    if(!fstr) {
+        perror("mem alloc fail");
+        fclose(file);
+        return 0;
+    }
+
+    fread(fstr, 1, file_size, file);
+    fstr[file_size] = '\0';
+
+    fclose(file);
+    printf("%s", fstr);
+    free(fstr);
+    return 0;
+}
+
+int newFile() {
+    printf("new");
+     return 0;
+}
+
+void startProgram() {
+    system("cls");
+    printf("-CEDIT-2025-\n");
+    char choice;
+    printf("(O) open file | (N) new file | (X) exit program\n");
+    scanf("%s", &choice);
+    switch(choice) {
+        case 'O':
+            openFile();
+            break;
+        case 'N':
+            newFile();
+            break;
+        case 'X':
+            printf("closeX");
+            break;
+        default:
+            printf("close");
+            break;
+    }
+}
 
 int main() {
-    printf("hello bro");
+    startProgram();
 
     return 0;
 }
